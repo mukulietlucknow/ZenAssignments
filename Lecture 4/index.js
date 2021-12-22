7b6e4cee429b9ef883a9d52b771c1547f2c49899
mukulietlucknow <mukul.ietlucknow@gmail.com>
2021-12-22 22:44:59 +0530

    lecture 4 task update

--- Lecture 4/index.js
+++ Lecture 4/index.js
@@ -36,16 +36,70 @@ req.onload = function(){
 
 // question number 3
 
-var req = new XMLHttpRequest();
-response = req.open('GET','https://restcountries.com/v2/all' ,true)
+var req2 = new XMLHttpRequest();
+response = req2.open('GET','https://restcountries.com/v2/all' ,true)
 
-req.send();
-req.onload = function(){
+req2.send();
+req2.onload = function(){
     var response = JSON.parse(req.response)
     for ( let i in response){
         console.log(response[i].name, " ",response[i].region," ", response[i].subregion," ", response[i].population  )
     }
 }
 
-// article i have read
+// 1. task from the medium.com url
+
+var var1, var2, var3;
+console.log(var1, var2, var3);
+
+// undefined undefined undefined
+
+// Q 2
+
+var myvar = 1;
+console.log(myvar);
+
+// Q 3
+
+var first_name = "mukul";
+var last_name = "varshney";
+var country = "india"
+var marital_status = "married"
+var age = 29
+
+
+// Q 4
+
+var [first_name,last_name,country,marital_status,age] = ["mukul","varshney", "india", "married", 29];
+
+
+// Q 5
+
+var [var1, var2, var3, var4] = ["mukul", true, undefined, null]
+
+// Q 6
+
+var num = "5"
+console.log(Number(num), +num, parseInt(num));
+
+
+// Q 7
+
+console.log(1==1, 2>1, 1<2, tru, false, 1)
+
+// Task 2: Simple Programs todo for Operators
+
+// Q 1
+
+console.log(2**2)
+
+// Q 2
+
+let a = 5, b = 6;
+[a, b] = [b, a];
+
+// Q 3
+
+
+
 
