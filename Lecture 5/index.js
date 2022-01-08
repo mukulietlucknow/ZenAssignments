72d31f0d058575e58636f35552cbb86182bf19ca
mukulietlucknow <mukul.ietlucknow@gmail.com>
2022-01-08 18:34:05 +0530

    update task

--- Lecture 5/index.js
+++ Lecture 5/index.js
@@ -1,3 +1,4 @@
+// i have mixed up the IIFE and normal fun and arrow function
 // print odd number
 var arr = [1,23,34,34,45,3,4,78,8]
 
@@ -20,4 +21,47 @@ var arr2 = ["mukul"];
     }
 })(arr2);
 
+// Sum of all numbers in an array
+
+var sum = arr => {
+    let sum = 0
+    for (let i of arr){
+        sum += i
+    }
+    return sum
+}
+
+console.log(sum([1,2,3]))
+
+// Return all the prime numbers in an array
+var isPrime = num => {
+    // number should be greater than 0 for this function
+    let arr = [0,1]
+    if (arr.includes(num)){
+        return false
+    }
+    let numHalf = Math.floor(num/2)
+    //console.log(numHalf)
+    if (numHalf == 1) {
+        return true
+    }
+    for (let i = 2 ; i <= numHalf ; i++){
+        if (num % i == 0){
+            return false
+        }
+    }
+    return true
+}
+
+var printPrime = arr => {
+    for ( let i of arr){
+        if (isPrime(i)){
+            console.log(i)
+        }
+    }
+}
+
+printPrime([1,2,3,4,5,6,7,8,9])
+
 // 
+
