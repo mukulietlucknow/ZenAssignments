bc377bda6f70a41a3f66ff94f4a773660b294966
mukulietlucknow <mukul.ietlucknow@gmail.com>
2022-01-21 20:19:48 +0530

    converted string to number

--- Lecture 5/index2.js
+++ Lecture 5/index2.js
@@ -79,10 +79,10 @@ var max = findMax(ar);
 console.log("Max: ", max);
 
 // Calculate the sum of numbers received in a comma delimited string
-console.log(sumCSV(“1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9”));
+console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
 function sumCSV(s)
 {
-    s = s.split(",");
+    s = s.split(",").map( num => Number(num));
     return s.reduce((a, b) => a + b, 0);
 }
 
