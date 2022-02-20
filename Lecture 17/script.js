e8ebf7f0224b2f87e8ebf592fc95b69eb89489b4
mukulietlucknow <mukul.ietlucknow@gmail.com>
2022-02-20 13:49:10 +0530

    Lecture 17

---
+++ Lecture 17/script.js
@@ -0,0 +1,44 @@
+setTimeout(() => {
+    var ele = document.getElementById("timer");
+    ele.innerText = 10
+    setTimeout(() =>{
+        var ele = document.getElementById("timer");
+        ele.innerText = 9
+        setTimeout(() =>{
+            var ele = document.getElementById("timer");
+            ele.innerText = 8
+            setTimeout(() =>{
+                var ele = document.getElementById("timer");
+                ele.innerText = 7
+                setTimeout(() =>{
+                    var ele = document.getElementById("timer");
+                    ele.innerText =6
+                    setTimeout(() =>{
+                        var ele = document.getElementById("timer");
+                        ele.innerText = 5
+                        setTimeout(() =>{
+                            var ele = document.getElementById("timer");
+                            ele.innerText = 4
+                            setTimeout(() =>{
+                                var ele = document.getElementById("timer");
+                                ele.innerText = 3
+                                setTimeout(() =>{
+                                    var ele = document.getElementById("timer");
+                                    ele.innerText = 2
+                                    setTimeout(() =>{
+                                        var ele = document.getElementById("timer");
+                                        ele.innerText = 1
+                                        setTimeout(() =>{
+                                            var ele = document.getElementById("timer");
+                                            ele.innerText = "happy independence day"
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000);
